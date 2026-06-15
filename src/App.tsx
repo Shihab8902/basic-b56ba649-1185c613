199563e3e01ffae77b7da9862d483c4be173e884
shihab8902 <shihabbd018@gmail.com>
2026-06-16 00:28:23 +0600

    Update App component with new text and fix formatting in package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@
 const App = () => {
   return (
     <div data-jc-id="Q6NCUB">
-            <p className="<span>This <span>This <span>This <span>This <span>This <span>This <span>This text is from jellycube, after I changed the GitHub flow</span> is from jellycube, after I changed the Git</span> is from jellycube, after I changed the Hit</span> is from jellycube, after I changed teh </span> is from jellycube, after I chang</span> is from jellycube, after I </span> is from jellycu</span>-base text-current opacity-80 leading-relaxed" data-jc-id="S22T3T">This text was added from Jellycube....</p>
-<h1 className="text-red-600" data-jc-id="Q6NCTE">This text was updated during deploy</h1>
-<span className="text-base text-gray-700 dark:text-gray-300" data-jc-id="RHTECM"><span>This text is from jellycueb</span></span>
+      <p className="<span>This <span>This <span>This <span>This <span>This <span>This <span>This text is from jellycube, after I changed the GitHub flow</span> is from jellycube, after I changed the Git</span> is from jellycube, after I changed the Hit</span> is from jellycube, after I changed teh </span> is from jellycube, after I chang</span> is from jellycube, after I </span> is from jellycu</span>-base text-current opacity-80 leading-relaxed" data-jc-id="S22T3T">This text was added from Jellycube....</p>
+      <h1 className="text-red-600" data-jc-id="Q6NCTE">This text was updated during deploy</h1>
+      <span className="text-base text-gray-700 dark:text-gray-300" data-jc-id="RHTECM"><span>This text is from jellycube</span></span>
+      <span className="text-base text-orange-700 dark:text-gray-300" data-jc-id="RHTECM"><span>This text is from VS code after github integration change.</span></span>
     </div>
   )
 }
