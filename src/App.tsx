e5167e62f79c55c715b41ddab11bb229fd369bea
Shihab Hasan <shihabbd018@gmail.com>
2026-06-16 00:23:24 +0600

    added new message

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@
 const App = () => {
   return (
     <div data-jc-id="Q6NCUB">
-            <p className="text-base text-current opacity-80 leading-relaxed" data-jc-id="S22T3T">This text was added from Jellycube....</p>
+            <p className="<span>This <span>This <span>This <span>This <span>This <span>This <span>This text is from jellycube, after I changed the GitHub flow</span> is from jellycube, after I changed the Git</span> is from jellycube, after I changed the Hit</span> is from jellycube, after I changed teh </span> is from jellycube, after I chang</span> is from jellycube, after I </span> is from jellycu</span>-base text-current opacity-80 leading-relaxed" data-jc-id="S22T3T">This text was added from Jellycube....</p>
 <h1 className="text-red-600" data-jc-id="Q6NCTE">This text was updated during deploy</h1>
+<span className="text-base text-gray-700 dark:text-gray-300" data-jc-id="RHTECM"><span>This text is from jellycueb</span></span>
     </div>
   )
 }
