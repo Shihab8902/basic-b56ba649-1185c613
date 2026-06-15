86da3c8879920da73fca814fe9694bd58fb46a55
Shihab Hasan <shihabbd018@gmail.com>
2026-06-16 00:22:24 +0600

    Initial commit — basic-b56ba649

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div data-jc-id="Q6NCUB">
+            <p className="text-base text-current opacity-80 leading-relaxed" data-jc-id="S22T3T">This text was added from Jellycube....</p>
+<h1 className="text-red-600" data-jc-id="Q6NCTE">This text was updated during deploy</h1>
+    </div>
+  )
+}
+
+export default App
